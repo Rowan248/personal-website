8a3e05eb65006f455b61db9c418bec70df68851a
Rowan248 <rowan.cusano@gmail.com>
2025-04-13 11:21:35 -0400

    Added buttons to exp that open the experiences to allow users to look at which one they want, made small css changes to NavBar and Skills

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -6,6 +6,8 @@ function Experience({ modalOpen }) {
 
   const [workExp, setWorkExp] = useState([]);
   const [otherExp, setOtherExp] = useState([]);
+  const [workExpBool, setWorkExpBool] = useState(false);
+  const [otherExpBool, setOtherExpBool] = useState(false);
 
   useEffect(() => {
     setWorkExp(experience.workExp);
@@ -20,18 +22,37 @@ function Experience({ modalOpen }) {
     modalOpen(otherExp[e.target.id - 1]);
   }
 
+  function handleButtonClick(button) {
+    if (button.target.id == 1) {
+      setWorkExpBool(!workExpBool);
+    }
+    else if (button.target.id == 2) {
+      setOtherExpBool(!otherExpBool);
+    }
+    else {
+      console.log("Not a valid button");
+    }
+  }
+
   return (
     <div className="experience">
       <div className="experiences work-experience">
-        {workExp.map((work) =>
-          <button id={work.id} key={work.id} className='exp-buttons' onClick={handleWorkModalOpen}>{work.title}</button>
-          // put div in button that takes up whole space and displays stuff
-        )}
+        <button id={1} className='exp-opener work-opener' onClick={handleButtonClick}>Work Experience</button>
+        {workExpBool && <div className='exp-block work-block'>
+          {workExp.map((work) =>
+            <button id={work.id} key={work.id} className='exp-buttons' onClick={handleWorkModalOpen}>{work.title}</button>
+            // put div in button that takes up whole space and displays stuff
+          )}
+        </div>}
       </div>
       <div className="experiences other-experience">
-        {otherExp.map((other) =>
-          <button id={other.id} key={other.id} className='exp-buttons' onClick={handleOtherModalOpen}>{other.title}</button>
-        )}
+        <button id={2} className='exp-opener other-opener' onClick={handleButtonClick}>Other Experience</button>
+        {otherExpBool && <div className='exp-block work-block'>
+          {otherExp.map((other) =>
+            <button id={other.id} key={other.id} className='exp-buttons' onClick={handleOtherModalOpen}>{other.title}</button>
+          )}
+        </div>}
+        {/* fix css here big time */}
       </div>
     </div>
   );

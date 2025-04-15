302b66f144fd8ba32b7207dfa30d2cf78dc684a6
Rowan248 <rowan.cusano@gmail.com>
2025-04-14 22:32:49 -0400

    Fixed base and package.json for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://rowan248.github.io/',
+  base: '/rowan248.github.io',
   plugins: [react()],
 })

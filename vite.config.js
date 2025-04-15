3ed638d60494da13d594b6836c7c66e5177eb276
Rowan248 <rowan.cusano@gmail.com>
2025-04-14 22:41:07 -0400

    Fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/rowan248.github.io',
+  base: '/personal-site/',
   plugins: [react()],
 })
